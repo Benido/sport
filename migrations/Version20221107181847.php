af6ecbfe72d34b247c57706c55093cf5ea928cef
Benido <remi.besnie@gmail.com>
2022-11-07 19:23:22 +0100

    Create a new migration file for setting up database

--- migrations/Version20221107181847.php
+++ migrations/Version20221107181847.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221030185149 extends AbstractMigration
+final class Version20221107181847 extends AbstractMigration
 {
     public function getDescription(): string
     {
